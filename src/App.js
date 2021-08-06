6b97f7031ac6835ed9256713c372efedd2f758a3
sarahartsoe <sarahartsoe@gmail.com>
2021-08-06 12:20:12 -0400

    start of dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg'
-import './App.css'
-import { AdminDashboard } from './components/admin'
+import logo from "./logo.svg";
+import "./App.css";
+import { AdminDashboard } from "./components/admin/AdminDashboard";
+import { Router } from "express";
+import { Switch } from "react-router";
 
-function App () {
+function App() {
   return (
-    
-  )
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/dashboard" component={AdminDashboard} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
