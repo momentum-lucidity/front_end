7a9f5624d0fb259043455fd45741cb4f2d9f9e7c
sarahartsoe <sarahartsoe@gmail.com>
2021-08-06 11:27:20 -0400

    created mock data files

---
+++ src/MockEvent.js
@@ -0,0 +1,112 @@
+[
+  {
+    id: 1,
+    title: 'Nina Takes a Lover',
+    description: 'Front-line national interface',
+    type: 'soccer',
+    date: '05/10/2020',
+    time: '12:39 AM',
+    location: '84 Maywood Way',
+    volunteer_slots: [
+      {
+        filled: true,
+        start_time: '8:52 AM',
+        end_time: '1:55 PM',
+        volunteer_name: 'Ashlee Cawsby',
+        user_id: 1
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Smiles of a Summer Night (Sommarnattens leende)',
+    description: 'Innovative user-facing methodology',
+    type: 'diaper drive',
+    date: '05/07/2021',
+    time: '12:15 PM',
+    location: '0 Sunnyside Alley',
+    volunteer_slots: [
+      {
+        filled: false,
+        start_time: '9:26 AM',
+        end_time: '10:32 AM',
+        volunteer_name: 'Tuck Bilsborrow',
+        user_id: 1
+      },
+      {
+        filled: true,
+        start_time: '3:08 AM',
+        end_time: '7:41 PM',
+        volunteer_name: 'Brook Loader',
+        user_id: 2
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Where the Trail Ends',
+    description: 'Extended 24 hour secured line',
+    type: 'soccer',
+    date: '28/03/2021',
+    time: '8:12 AM',
+    location: '721 Eliot Plaza',
+    volunteer_slots: [
+      {
+        filled: false,
+        start_time: '4:20 AM',
+        end_time: '12:18 AM',
+        volunteer_name: 'Justinian Fitzsimmons',
+        user_id: 1
+      },
+      {
+        filled: true,
+        start_time: '1:19 PM',
+        end_time: '9:34 PM',
+        volunteer_name: 'Debra Bust',
+        user_id: 2
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'Statue of Liberty, The',
+    description: 'Cross-group background matrices',
+    type: 'ESL Class',
+    date: '16/09/2020',
+    time: '2:38 AM',
+    location: '60 Pepper Wood Hill',
+    volunteer_slots: []
+  },
+  {
+    id: 5,
+    title: 'Panic',
+    description: 'Enterprise-wide system-worthy project',
+    type: 'soccer',
+    date: '15/05/2021',
+    time: '8:38 AM',
+    location: '9 Caliangt Alley',
+    volunteer_slots: [
+      {
+        filled: true,
+        start_time: '8:04 PM',
+        end_time: '2:21 AM',
+        volunteer_name: 'Connie Shaw',
+        user_id: 1
+      },
+      {
+        filled: true,
+        start_time: '4:39 AM',
+        end_time: '12:25 AM',
+        volunteer_name: 'Natka Elsay',
+        user_id: 2
+      },
+      {
+        filled: false,
+        start_time: '5:21 AM',
+        end_time: '7:36 PM',
+        volunteer_name: 'Jae Pund',
+        user_id: 3
+      }
+    ]
+  }
+]
---
+++ src/MockVolunteers.js
@@ -0,0 +1,192 @@
+[
+  {
+    id: 1,
+    preferred_name: 'Melli',
+    legal_name: 'Melli Ianizzi',
+    email: 'mianizzi0@homestead.com',
+    gender: 'Genderfluid',
+    is_admin: true,
+    address: [
+      {
+        street: '1229 Bartillon Road',
+        city: 'New York City',
+        state: 'New York',
+        postal: '10105'
+      }
+    ],
+    intake_status: 3,
+    preferred_language: 'Spanish',
+    phone: '338-300-9751'
+  },
+  {
+    id: 2,
+    preferred_name: 'Rockie',
+    legal_name: 'Rockie Bahia',
+    email: 'rbahia1@mit.edu',
+    gender: 'Non-binary',
+    is_admin: false,
+    address: [
+      {
+        street: '148 Hermina Pass',
+        city: 'Inglewood',
+        state: 'California',
+        postal: '90305'
+      }
+    ],
+    intake_status: 1,
+    preferred_language: 'English',
+    phone: '910-850-8632'
+  },
+  {
+    id: 3,
+    preferred_name: 'Oralee',
+    legal_name: 'Oralee Haizelden',
+    email: 'ohaizelden2@uiuc.edu',
+    gender: 'Genderfluid',
+    is_admin: false,
+    address: [
+      {
+        street: '85870 Sunbrook Street',
+        city: 'Springfield',
+        state: 'Illinois',
+        postal: '62711'
+      }
+    ],
+    intake_status: 3,
+    preferred_language: 'English',
+    phone: '488-900-6156'
+  },
+  {
+    id: 4,
+    preferred_name: 'Alasteir',
+    legal_name: 'Alasteir Dadley',
+    email: 'adadley3@networksolutions.com',
+    gender: 'Bigender',
+    is_admin: true,
+    address: [
+      {
+        street: '90643 American Ash Way',
+        city: 'Honolulu',
+        state: 'Hawaii',
+        postal: '96835'
+      }
+    ],
+    intake_status: 1,
+    preferred_language: 'Spanish',
+    phone: '800-485-5637'
+  },
+  {
+    id: 5,
+    preferred_name: 'Dan',
+    legal_name: 'Dan Gilbard',
+    email: 'dgilbard4@imageshack.us',
+    gender: 'Male',
+    is_admin: false,
+    address: [
+      {
+        street: '066 Sachs Crossing',
+        city: 'Lakewood',
+        state: 'Washington',
+        postal: '98498'
+      }
+    ],
+    intake_status: 3,
+    preferred_language: 'Spanish',
+    phone: '559-180-4662'
+  },
+  {
+    id: 6,
+    preferred_name: 'Elisha',
+    legal_name: 'Elisha Gerrett',
+    email: 'egerrett5@goo.ne.jp',
+    gender: 'Polygender',
+    is_admin: false,
+    address: [
+      {
+        street: '83 Del Sol Pass',
+        city: 'Seminole',
+        state: 'Florida',
+        postal: '34642'
+      }
+    ],
+    intake_status: 1,
+    preferred_language: 'Spanish',
+    phone: '279-814-6949'
+  },
+  {
+    id: 7,
+    preferred_name: 'Ursa',
+    legal_name: 'Ursa Lasty',
+    email: 'ulasty6@usatoday.com',
+    gender: 'Genderfluid',
+    is_admin: false,
+    address: [
+      {
+        street: '33 Forest Run Court',
+        city: 'Detroit',
+        state: 'Michigan',
+        postal: '48217'
+      }
+    ],
+    intake_status: 1,
+    preferred_language: 'English',
+    phone: '985-613-6171'
+  },
+  {
+    id: 8,
+    preferred_name: 'Gaspar',
+    legal_name: 'Gaspar Shenton',
+    email: 'gshenton7@freewebs.com',
+    gender: 'Female',
+    is_admin: false,
+    address: [
+      {
+        street: '7 Forest Crossing',
+        city: 'Fort Lauderdale',
+        state: 'Florida',
+        postal: '33325'
+      }
+    ],
+    intake_status: 1,
+    preferred_language: 'English',
+    phone: null
+  },
+  {
+    id: 9,
+    preferred_name: 'Codie',
+    legal_name: 'Codie Doornbos',
+    email: 'cdoornbos8@unicef.org',
+    gender: 'Genderqueer',
+    is_admin: false,
+    address: [
+      {
+        street: '8541 Dottie Trail',
+        city: 'Albany',
+        state: 'New York',
+        postal: '12237'
+      }
+    ],
+    intake_status: 2,
+    preferred_language: 'English',
+    phone: '961-134-2686'
+  },
+  {
+    id: 10,
+    preferred_name: 'Kienan',
+    legal_name: 'Kienan Glasheen',
+    email: 'kglasheen9@wix.com',
+    gender: 'Male',
+    is_admin: false,
+    address: [
+      {
+        street: '499 Merry Parkway',
+        city: 'Lancaster',
+        state: 'California',
+        postal: '93584'
+      }
+    ],
+    intake_status: 2,
+    preferred_language: 'English',
+    phone: '775-771-0363'
+  }
+]
