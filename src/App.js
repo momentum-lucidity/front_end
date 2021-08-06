8e7523ee04143098d997b5168c018a97b35685ca
sarahartsoe <sarahartsoe@gmail.com>
2021-08-06 14:32:54 -0400

    fix for netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { AdminDashboard } from "./components/admin/AdminDashboard";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
