f62cc8497982a5663e2ebb6a8997c0ec8917aa08
loganatkinson <logan.atkinson@gmail.com>
2021-08-09 11:23:00 -0400

    added registration form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import { AdminDashboard } from './components/admin/AdminDashboard';
-import { AdminLogin } from './components/admin/AdminLogin';
-import { VolunteerList } from './components/admin/VolunteerList';
-import { EventsList } from './components/EventsList';
-import { DocumentList } from './components/admin/DocumentList';
-import { CreateAnnouncements } from './components/admin/CreateAnnouncements';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'tailwindcss/tailwind.css';
+import './App.css'
+import { AdminDashboard } from './components/admin/AdminDashboard'
+import { AdminLogin } from './components/admin/AdminLogin'
+import { VolunteerList } from './components/admin/VolunteerList'
+import { EventsList } from './components/EventsList'
+import { DocumentList } from './components/admin/DocumentList'
+import { Registration } from './components/Registration'
+import { CreateAnnouncements } from './components/admin/CreateAnnouncements'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'tailwindcss/tailwind.css'
 
 function App () {
   return (
@@ -14,6 +15,7 @@ function App () {
       <div className='App'>
         <Switch>
           <Route exact path='/' component={AdminLogin} />
+          <Route path='/registration' component={Registration} />
           <Route exact path='/admindash' component={AdminDashboard} />
           <Route path='/volunteers' component={VolunteerList} />
           <Route path='/events' component={EventsList} />
