81b62196ca0607267687bdb9e535b0c0a59d66b7
Greg Sronce <greg.sronce@gmail.com>
2021-08-18 11:48:02 -0400

    Have ID actually targeted on announcements page

--- src/components/admin/AnnouncementsList.js
+++ src/components/admin/AnnouncementsList.js
@@ -6,14 +6,16 @@ import { Dialog, Menu, Transition } from '@headlessui/react'
 import { getAnnouncements, deleteAnnouncement } from '../../api'
 import Avatar from 'react-avatar'
 import { PencilIcon, ChevronRightIcon, CalendarIcon, FolderIcon, HomeIcon, InboxIcon, MenuAlt2Icon, UsersIcon, XIcon, TrashIcon } from '@heroicons/react/outline'
+import { useHistory } from 'react-router-dom'
 
 export const AnnouncementsList = (props) => {
   const { token, authUser } = props
   const [announcements, setAnnouncements] = useState([])
   const [currentPage] = useState(1)
   const [announcementsPerPage] = useState(5)
-  const [selectedPK, setSelectedPK] = useState('')
+  const [selectedPK, setSelectedPK] = useState(null)
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const history = useHistory()
 
   useEffect(() => {
     getAnnouncements()
@@ -32,19 +34,22 @@ export const AnnouncementsList = (props) => {
   const indexOfFirstAnnouncement = indexOfLastAnnouncement - announcementsPerPage;
   const currentAnnouncements = sortedAnnouncements.slice(indexOfFirstAnnouncement, indexOfLastAnnouncement)
   
-  const handleDelete = () => {
-    deleteAnnouncement(selectedPK)
-  }
+  const handleDelete = async () => {
+    const deleteWorked = await deleteAnnouncement(selectedPK)
+    if (deleteWorked) {
+       history.push('/announcements')}
+    }
 
   const setPK = (event) => {
-    setSelectedPK(event.target.value)
+    if (event.target.className)setSelectedPK(event.target.value)
     console.log(selectedPK)
   }
-  const handlePK = (event) => {
-    setSelectedPK(event.target.value)
-    handleDelete()
-    console.log(selectedPK)
-  }
+
+  // const handlePK = async (event) => {
+  //   setSelectedPK(event.target.value)
+  //   handleDelete()
+  //   console.log(selectedPK)
+  // }
 
   const navigation = [
     { name: 'Dashboard', href: '/admindash', icon: HomeIcon, current: false },
@@ -334,7 +339,20 @@ export const AnnouncementsList = (props) => {
                         <button
                           type='button'
                           className='inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500'
-                          onClick={setPK}
+                          onClick={() => setSelectedPK(announcement.alertpk)}  
+                        >
+                          <PencilIcon
+                            className='-ml-1 mr-2 h-5 w-5 text-gray-400'
+                            aria-hidden='true'
+                          />
+                          Select Announcement
+                        </button>
+                      </span>
+                      <span className='hidden sm:block'>
+                        <button
+                          type='button'
+                          className='inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500'
+                          onClick={setPK} 
                           value={announcement.alertpk}
                         >
                           <PencilIcon
@@ -349,7 +367,8 @@ export const AnnouncementsList = (props) => {
                           type='button'
                           value={announcement.alertpk}
                           className='inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500'
-                          onClick={handlePK}
+                          onClick={handleDelete}                     
+
                         >
                           <TrashIcon
                             className='-ml-1 mr-2 h-5 w-5 text-gray-400'
@@ -357,7 +376,7 @@ export const AnnouncementsList = (props) => {
                           />
                           Delete
                         </button>
-                      </span>
+                      </span> 
                     </div>
                   </div>
                 </li>
