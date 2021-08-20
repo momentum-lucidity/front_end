26ff653d9fd10017fa11ebc0a0328330ddb38b2c
Greg Sronce <greg.sronce@gmail.com>
2021-08-19 20:32:32 -0400

    finally figured out on event capture

--- src/components/admin/DocumentList.js
+++ src/components/admin/DocumentList.js
@@ -336,7 +336,7 @@ export const DocumentList = (props) => {
                               )}
                             ></div>
                             <div className="flex-1 flex-row items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                              <div className="flex justify-between px-4 py-2 text-sm truncate">
+                              <div onClickCapture={() => setDocumentPK(document.docpk)} className="flex justify-between px-4 py-2 text-sm truncate">
                                 <a
                                   href={document.url}
                                   className="text-gray-900 font-medium hover:text-gray-600"
@@ -345,13 +345,13 @@ export const DocumentList = (props) => {
                                 >
                                   {document.doc_header}
                                 </a>
-                                <button>      
+                                {/* <button>      
                                 <TrashIcon
                                   className="-ml-1 mr-2 h-5 w-5"
                                   aria-hidden="true"
                                   onClick={() => setDocumentPK(document.docpk)}
                                 />
-                                </button>   
+                                </button>    */}
                                 <button>      
                                 <TrashIcon
                                   className="-ml-1 mr-2 h-5 w-5"
