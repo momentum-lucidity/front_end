2b0983df265fae5b958d6881d2f9f333f0bb83c6
loganatkinson <logan.atkinson@gmail.com>
2021-08-20 18:52:56 -0400

    switched list of upcoming events into seperate cards to make it look more clickable

--- src/components/admin/EventsList.js
+++ src/components/admin/EventsList.js
@@ -290,71 +290,66 @@ export const EventsList = (props) => {
 
         <main className='flex-1 relative overflow-y-auto focus:outline-none'>
           <div className='py-6'>
-            <div className='px-4 pb-5 sm:px-6 md:px-0'>
-              <h1 className='text-2xl font-semibold text-gray-900'>
-                Upcoming Events
-              </h1>
-            </div>
-            <div className='ml-4 mt-2 pb-4 flex-shrink-0'>
-              <Link to='/events/eventform'>
-                <button
-                  type='button'
-                  className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                >
-                  Create new event
-                </button>
-              </Link>
-            </div>
-            <div className='px-4 sm:px-6 md:px-0'>
-              <ul className='divide-y divide-gray-200'>
-                {allEvents && allEvents.map((event, idx) => (
-                  <li
-                    key={event.eventpk}
-                    className='relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600'
+            <div className='pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between'>
+              <h3 className='text-xl leading-6 font-medium text-gray-900'>Upcoming Events</h3>
+              <div className='mt-3 sm:mt-0 sm:ml-4'>
+                <Link to='/events/eventform'>
+                  <button
+                    type='button'
+                    className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                   >
-                    <div className='flex justify-between space-x-3'>
-                      <div className='min-w-0 flex-1'>
-                        <Link
-                          to={{
+                    New Event
+                  </button>
+                </Link>
+              </div>
+            </div>
 
-                            pathname: `/events/${event.eventpk}/`,
-                            state: { event: event }
-                          }}
+            <div className='grid grid-cols-1 gap-4 sm:grid-cols-1'>
+              {allEvents && allEvents.map((event, idx) => (
+                <div key={event.idx}>
+                  <div
+                    key={event.idx}
+                    className='relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'
+                  >
+                    <div className='flex-shrink-0' id='img_holder' />
+                    <div className='grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
+                      <Link
+                        to={{
+
+                          pathname: `/events/${event.eventpk}/`,
+                          state: { event: event }
+                        }}
+                      >
+                        <a
+                          href={`/events/${event.eventpk}`}
+                          className='block focus:outline-none'
                         >
-                          <a
-                            href={`/events/${event.eventpk}`}
-                            className='block focus:outline-none'
-                          >
-                            <span
-                              className='absolute inset-0'
-                              aria-hidden='true'
-                            />
-                            <p className='text-sm font-medium text-gray-900 truncate'>
-                              {event.event_header}
-                            </p>
-                            <p className='text-sm text-gray-500 truncate'>
+                          <p className='text-sm font-medium text-gray-900 truncate'>
+                            {event.event_header}
+                          </p>
+                          <div className='grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2'>
+                            <p className='sm:col-span-1 text-sm text-gray-500 truncate'>
                               {event.date}
                             </p>
-                            <p className='text-sm text-gray-500 truncate'>
+                            <p className='sm:col-span-1 text-sm text-gray-500 truncate'>
                               {event.time}
                             </p>
-                            <p className='line-clamp-2 text-sm text-gray-600'>
-                              {event.location}
-                            </p>
-                            <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800'>
+                            <div className='sm:col-span-1 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800'>
                               Volunteers Needed
-                            </span>
-                            <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800'>
+                            </div>
+                            <div className='sm:col-span-1 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800'>
                               Volunteers Signed Up
-                            </span>
-                          </a>
-                        </Link>
-                      </div>
+                            </div>
+                          </div>
+
+                        </a>
+                      </Link>
                     </div>
-                  </li>
-                ))}
-              </ul>
+                  </div>
+                </div>
+              ))}
             </div>
+
           </div>
         </main>
       </div>
