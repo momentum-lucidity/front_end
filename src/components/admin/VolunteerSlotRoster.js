95dfdba1aa110d36145d3bd91542fc1469185de6
loganatkinson <logan.atkinson@gmail.com>
2021-08-20 12:43:13 -0400

    delete slot working

--- src/components/admin/VolunteerSlotRoster.js
+++ src/components/admin/VolunteerSlotRoster.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { getAllSlots } from '../../api'
+import { Link } from 'react-router-dom'
+import { deleteVolunteerSlot, getAllSlots } from '../../api'
 import Avatar from 'react-avatar'
 import { VolunteerSlotDelete } from './VolunteerSlotDelete'
 import { Switch } from '@headlessui/react'
@@ -7,14 +8,11 @@ import { Switch } from '@headlessui/react'
 export const VolunteerSlotRoster = (props) => {
   const { token, eventDetails, allVolunteers } = props
   const [allVSlots, setAllVSlots] = useState([])
+  const [selectedSlotID, setSelectedSlotID] = useState('')
   const [enabled, setEnabled] = useState(false)
   const fetchedAllSlots = useRef(false)
   const eventID = eventDetails.eventpk
   console.log(`eventID - ${eventID}`)
-
-  const filteredSlots = allVSlots.filter(slot => {
-    return (slot.event === eventID)
-  })
 
   function classNames (...classes) {
     return classes.filter(Boolean).join(' ')
@@ -28,6 +26,18 @@ export const VolunteerSlotRoster = (props) => {
     }
   }, [allVSlots, token])
 
+  const filteredSlots = allVSlots.filter(slot => {
+    return (slot.event === eventID)
+  })
+
+  const handleDelete = async () => {
+    const success = await deleteVolunteerSlot(token, selectedSlotID)
+    if (success) {
+      getAllSlots()
+        .then((data) => setAllVSlots(data))
+    }
+  }
+  console.log(`this is what you are looking for ${selectedSlotID}`)
   return (
     <div className='flex flex-col'>
       <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
@@ -67,34 +77,73 @@ export const VolunteerSlotRoster = (props) => {
               </thead>
               <tbody className='bg-white divide-y divide-gray-200'>
                 {filteredSlots.map((slot) => (
-                  <tr
-                    key={slot.slotpk}
+                  <div key={slot.slotpk} onClickCapture={() => setSelectedSlotID(slot.slotpk)}>
+                    <tr key={slot.slotpk}>
 
-                  >
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='flex items-center'>
-                        <div className='flex-shrink-0 h-10 w-10'>
-                          <Avatar name={slot.user} size='40' round />
+                      <td className='px-6 py-4 whitespace-nowrap'>
+
+                        <div className='flex items-center'>
+
+                          <div className='flex-shrink-0 h-10 w-10'>
+                            <Avatar name={slot.user} size='40' round />
+                          </div>
+
+                          <div className='ml-4'>
+                            <div className='text-sm font-medium text-gray-900'>event_header:{eventDetails.event_header}</div>
+                            <div className='text-sm text-gray-500'>this is the slotpk:{slot.slotpk}</div>
+                          </div>
 
                         </div>
-                        <div className='ml-4'>
-                          <div className='text-sm font-medium text-gray-900'>event_header:{eventDetails.event_header}</div>
-                          <div className='text-sm text-gray-500'>this is the slotpk:{slot.slotpk}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <div className='text-sm text-gray-900'>user id:{slot.user}</div>
-                      <div className='text-sm text-gray-500'>vlot_text:{slot.vslot_text}</div>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap'>
-                      <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
-                        slot.event id: {slot.event}
-                        event ID : {eventID}
+
+                      </td>
+
+                      <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-900'>user id:{slot.user}</div>
+                        <div className='text-sm text-gray-500'>vlot_text:{slot.vslot_text}</div>
+                      </td>
+
+                      <td className='px-6 py-4 whitespace-nowrap'>
+
+                        <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+                          slot.event id: {slot.event}
+                          event ID : {eventID}
+                        </span>
+
+                      </td>
+
+                      <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>event type:{eventDetails.type}</td>
+
+                      <span className='hidden sm:block'>
+
+                        <Link
+                          to='#'
+                        >
+
+                          <button
+                            type='button'
+                            className='inline-flex items-center px-4 py-2 border border-green-200 rounded-md shadow-sm text-sm font-medium text-black bg-transparent hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-green-200'
+                          >
+                            Edit
+                          </button>
+
+                        </Link>
+
                       </span>
-                    </td>
-                    <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>event type:{eventDetails.type}</td>
-                  </tr>
+
+                      <span className='hidden sm:block'>
+
+                        <button
+                          type='button'
+                          className='inline-flex items-center px-4 py-2 border border-red-300 rounded-md shadow-sm text-sm font-medium text-black bg-transparent hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
+                          onClick={handleDelete}
+                        >
+                          Delete
+                        </button>
+
+                      </span>
+
+                    </tr>
+                  </div>
                 ))}
               </tbody>
             </table>
