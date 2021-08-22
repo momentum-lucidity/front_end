a07a116d6a2fbe459a8d9b205d3801a331e64556
Greg Sronce <greg.sronce@gmail.com>
2021-08-22 11:04:39 -0400

    Updated volunteer login error message

--- src/components/volunteer/VolunteerLogin.js
+++ src/components/volunteer/VolunteerLogin.js
@@ -1,104 +1,141 @@
-import { LockClosedIcon } from '@heroicons/react/solid'
-import { useState } from 'react'
-import { useHistory } from 'react-router'
-import { requestLogin } from '../../api'
+import { LockClosedIcon, XCircleIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { requestLogin } from "../../api";
 
 export const VolunteerLogin = (props) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const { setToken, setErrors, errors } = props
-  const history = useHistory()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setToken, setErrors, errors } = props;
+  const history = useHistory();
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     requestLogin(username, password)
-      .then((data) => {setToken(data.data.auth_token)
-    history.push('/dreamcenter/volunteerdash')})
-    .catch((error) => {setErrors(error.message)})
-  }
+      .then((data) => {
+        setToken(data.data.auth_token);
+        history.push("/dreamcenter/volunteerdash");
+      })
+      .catch((error) => {
+        setErrors(error.message);
+      });
+  };
 
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
-      <div className='max-w-md w-full space-y-8'>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
           <img
-            className='mx-auto h-12 w-auto'
-            src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-            alt='Workflow'
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
           />
-          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Sign in to your account</h2>
-          <p className='mt-2 text-center text-sm text-gray-600'>
-            Or{' '}
-            <a href='/registration' className='font-medium text-indigo-600 hover:text-indigo-500'>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{" "}
+            <a
+              href="/registration"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
               Register for a new account
             </a>
           </p>
         </div>
-        <form className='mt-8 space-y-6' action='#' method='POST' onSubmit={handleSubmit}>
-        { errors && <div className='text-red-600'>Login Failed: Your user ID or password is incorrect. <br /> Please try logging in again.</div>}
-          <input type='hidden' name='remember' defaultValue='true' />
-          <div className='rounded-md shadow-sm -space-y-px'>
+        <form
+          className="mt-8 space-y-6"
+          action="#"
+          method="POST"
+          onSubmit={handleSubmit}
+        >
+          {errors && (
+            <div className="rounded-md bg-red-50 p-4 mt-2">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <XCircleIcon
+                    className="h-5 w-5 text-red-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="ml-3">
+                  <h3 className="text-sm font-medium text-red-800">
+                    Login Failed: Your user ID or password is incorrect.
+                  </h3>
+                </div>
+              </div>
+            </div>
+          )}{" "}
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor='username' className='sr-only'>
+              <label htmlFor="username" className="sr-only">
                 username
               </label>
               <input
-                id='username'
-                name='username'
-                type='username'
+                id="username"
+                name="username"
+                type="username"
                 value={username}
                 onChange={(event) => setUsername(event.target.value)}
-                autoComplete='username'
+                autoComplete="username"
                 required
-                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                placeholder='username'
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="username"
               />
             </div>
             <div>
-              <label htmlFor='password' className='sr-only'>
+              <label htmlFor="password" className="sr-only">
                 Password
               </label>
               <input
-                id='password'
-                name='password'
-                type='password'
+                id="password"
+                name="password"
+                type="password"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
-                autoComplete='current-password'
+                autoComplete="current-password"
                 required
-                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                placeholder='Password'
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
               />
             </div>
           </div>
-
-          <div className='flex items-center justify-between'>
-            <div className='flex items-center'>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
               <input
-                id='remember-me'
-                name='remember-me'
-                type='checkbox'
-                className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
               />
-              <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-900'>
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
                 Remember me
               </label>
             </div>
 
-            <div className='text-sm'>
-              <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
                 Forgot your password?
               </a>
             </div>
           </div>
-
           <div>
             <button
-              type='submit'
-              className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
-                <LockClosedIcon className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400' aria-hidden='true' />
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <LockClosedIcon
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  aria-hidden="true"
+                />
               </span>
               Sign in
             </button>
@@ -106,5 +143,5 @@ export const VolunteerLogin = (props) => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
