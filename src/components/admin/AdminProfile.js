cc06f02ad5f544428233a0140c20b5c8053b5082
loganatkinson <logan.atkinson@gmail.com>
2021-08-22 19:41:17 -0400

    added logo to adminProfile and styling to make it cohesive

--- src/components/admin/AdminProfile.js
+++ src/components/admin/AdminProfile.js
@@ -1,17 +1,8 @@
-import { Fragment, useState } from 'react';
-import Avatar from 'react-avatar';
-import { Dialog, Menu, Transition } from '@headlessui/react';
-
-import {
-  ChevronRightIcon,
-  CalendarIcon,
-  FolderIcon,
-  HomeIcon,
-  InboxIcon,
-  MenuAlt2Icon,
-  UsersIcon,
-  XIcon
-} from '@heroicons/react/outline';
+import { Fragment, useState } from 'react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
+import Logo from '../images/1x/logo.png'
+import Avatar from 'react-avatar'
+import { ChevronRightIcon, CalendarIcon, FolderIcon, HomeIcon, InboxIcon, MenuAlt2Icon, UsersIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '/admindash', icon: HomeIcon, current: true },
@@ -43,7 +34,7 @@ export const AdminProfile = (props) => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
   console.log(authUser)
   return (
-    <div className='h-screen bg-white overflow-hidden flex pb-2'>
+    <div className='h-screen bg-gray-50 overflow-hidden flex'>
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as='div'
@@ -61,7 +52,7 @@ export const AdminProfile = (props) => {
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <Dialog.Overlay className='fixed inset-0 bg-gray-600 bg-opacity-75' />
+            <Dialog.Overlay className='fixed inset-0 bg-gray-700 bg-opacity-75' />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
@@ -92,10 +83,13 @@ export const AdminProfile = (props) => {
                   </button>
                 </div>
               </Transition.Child>
-              <div className='flex-shrink-0 px-4 flex items-center'>
-                {/* logo here */}
+              <div className='sm:mx-auto sm:w-9/12 sm:max-h-4'>
+                <img
+                  src={Logo}
+                  alt='lucidity'
+                />
               </div>
-              <div className='mt-5 flex-1 h-0 overflow-y-auto'>
+              <div className='mt-5 flex-1 h-0 bg-white overflow-y-auto'>
                 <nav className='px-2 space-y-1'>
                   {navigation.map((item) => (
                     <a
@@ -124,17 +118,21 @@ export const AdminProfile = (props) => {
               </div>
             </div>
           </Transition.Child>
-          <div className='flex-shrink-0 w-14'>
+          <div className=' bg-white flex-shrink-0 w-14'>
             {/* Dummy element to force sidebar to shrink to fit close icon */}
           </div>
         </Dialog>
       </Transition.Root>
 
       <div className='hidden md:flex md:flex-shrink-0'>
-        <div className='w-64 flex flex-col'>
-          <div className='border-r border-gray-300 pt-5 pb-4 flex flex-col flex-grow overflow-y-auto'>
-            <div className='flex-shrink-0 px-4 flex items-center'>
-              {/* logo here */}
+        <div className='w-64 bg-white flex flex-col'>
+          <div className='border-r border-gray-200 pt-5 pb-4 flex flex-col flex-grow overflow-y-auto'>
+            <div className='flex-shrink-1 px-4 flex items-center'>
+              <img
+                className='w-full, bg-white'
+                src={Logo}
+                alt='Lucidity Logo'
+              />
             </div>
             <div className='flex-grow mt-5 flex flex-col'>
               <nav className='flex-1 bg-white px-2 space-y-1'>
@@ -167,22 +165,22 @@ export const AdminProfile = (props) => {
         </div>
       </div>
       <div className='flex-1 max-w-4xl mx-auto w-0 flex flex-col md:px-8 xl:px-0'>
-        <div className='relative z-10 flex-shrink-0 h-16 bg-white border-b border-gray-300 flex'>
+        <div className='relative z-10 flex-shrink-0 h-24 bg-gray-50 border-b border-gray-200 flex'>
           <button
-            className='border-r border-gray-300 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden'
+            className='border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden'
             onClick={() => setSidebarOpen(true)}
           >
             <span className='sr-only'>Open sidebar</span>
             <MenuAlt2Icon className='h-6 w-6' aria-hidden='true' />
           </button>
-          <div className='flex-1 flex'>
+          <div className='flex-1 flex bg-gray-50'>
             <nav className='flex' aria-label='Breadcrumb'>
               <ol className='flex items-center space-x-4'>
                 <li>
                   <div>
-                    <a href='/' className='text-gray-400 hover:text-gray-500'>
+                    <a href='/' className='text-gray-700 hover:text-indigo-500'>
                       <HomeIcon
-                        className='flex-shrink-0 h-5 w-5'
+                        className='flex-shrink-0 h-7 w-7'
                         aria-hidden='true'
                       />
                       <span className='sr-only'>Home</span>
@@ -193,12 +191,12 @@ export const AdminProfile = (props) => {
                   <li key={page.name}>
                     <div className='flex items-center'>
                       <ChevronRightIcon
-                        className='flex-shrink-0 h-5 w-5 text-gray-400'
+                        className='flex-shrink-0 h-8 w-8 text-gray-700'
                         aria-hidden='true'
                       />
                       <a
                         href={page.href}
-                        className='ml-4 text-sm font-medium text-gray-500 hover:text-gray-700'
+                        className='ml-4 text-md font-medium text-gray-700 hover:text-indigo-500'
                         aria-current={page.current ? 'page' : undefined}
                       >
                         {page.name}
@@ -209,14 +207,14 @@ export const AdminProfile = (props) => {
               </ol>
             </nav>
           </div>
-          <div className='ml-4 flex items-center md:ml-6'>
+          <div className='ml-4 flex items-center bg-gray-50 md:ml-6'>
             <Menu as='div' className='ml-3 relative'>
               {({ open }) => (
                 <>
                   <div>
                     <Menu.Button className='max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                       <span className='sr-only'>Open user menu</span>
-                      <Avatar name={authUser.legal_name} size='40' round />
+                      <Avatar name={authUser.legal_name} size='60' round />
                     </Menu.Button>
                   </div>
                   <Transition
@@ -256,7 +254,7 @@ export const AdminProfile = (props) => {
           </div>
         </div>
 
-        <div className='bg-white shadow overflow-hidden sm:rounded-lg overflow-y-auto pb-4'>
+        <div className='bg-white shadow overflow-hidden sm:rounded-xl mt-16 mb-8 overflow-y-auto pb-4'>
           <div className='px-4 py-5 sm:px-6'>
             <h1 className='text-2xl font-semibold text-gray-900'>
               {authUser.display_name}'s Profile
@@ -361,4 +359,4 @@ export const AdminProfile = (props) => {
       </div>
     </div>
   )
-};
+}
