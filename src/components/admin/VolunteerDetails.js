0a1fa328c80d500339fe4c984e93964963ec6db0
loganatkinson <logan.atkinson@gmail.com>
2021-08-25 08:58:25 -0400

    switched volunteer profile to more of a display card

--- src/components/admin/VolunteerDetails.js
+++ src/components/admin/VolunteerDetails.js
@@ -1,6 +1,7 @@
 import { Fragment, useEffect, useState } from 'react'
 import { Link, useHistory, useParams } from 'react-router-dom'
 import { getUserDetails, deleteUser, getAllSlots } from '../../api'
+import { ProfileHeader } from './ProfileHeader.js'
 import { Dialog, Menu, Transition } from '@headlessui/react'
 import { VolunteerIntakeStatus } from './VolunteerIntakeStatus'
 import Avatar from 'react-avatar'
@@ -14,6 +15,7 @@ import {
   MenuAlt2Icon,
   UsersIcon,
   XIcon,
+  DotsVerticalIcon,
   PaperClipIcon,
   TrashIcon,
   PencilIcon
@@ -287,64 +289,40 @@ export const VolunteerDetails = (props) => {
         </div>
         <main className='flex-1 mt-8 relative overflow-y-auto focus:outline-none'>
           <div className='bg-white shadow overflow-hidden sm:rounded-lg'>
-            <div className='px-4 py-5 sm:px-6'>
-              <h3 className='text-lg leading-6 font-medium text-gray-900'>{userDetails.legal_name}'s</h3>
-              <p className='mt-1 max-w-2xl text-sm text-gray-500'>Volunteer Profile</p>
+            <div className='px-4 py-6 sm:px-12'>
+              <ProfileHeader userDetails={userDetails} setUserDetails={setUserDetails} id={id} token={token} />
             </div>
-            <div className='border-t border-gray-200 px-4 py-5 sm:px-6'>
+            <div className='border-t border-gray-200 px-4 py-5 sm:px-12'>
               <dl className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
                 <div className='sm:col-span-1'>
-                  <dt className='text-sm font-medium text-gray-500'>Full name</dt>
-                  <dd className='mt-1 text-sm text-gray-900'>{userDetails.legal_name}</dd>
+                  <dt className='text-md font-medium text-gray-500'>Full name</dt>
+                  <dd className='mt-1 text-sm text-gray-900'>Legal Name: {userDetails.legal_name}</dd>
+                  <dd className='mt-1 text-sm text-gray-900'>Username: {userDetails.username}</dd>
                 </div>
                 <div className='sm:col-span-1'>
-                  <dt className='text-sm font-medium text-gray-500'>Application for</dt>
-                  <dd className='mt-1 text-sm text-gray-900'>Backend Developer</dd>
+                  <dt className='text-md font-medium text-gray-500'>Personal Identifiers</dt>
+                  <dd className='mt-1 text-sm text-gray-900'>Preferred Name: {userDetails.display_name}</dd>
+                  <dd className='mt-1 text-sm text-gray-900'>Pronouns: {userDetails.pronouns}</dd>
                 </div>
                 <div className='sm:col-span-1'>
-                  <dt className='text-sm font-medium text-gray-500'>Email address</dt>
+                  <dt className='text-md font-medium text-gray-500'>Contact Information</dt>
+                  <dd className='mt-1 text-sm text-gray-900'>{userDetails.telephone}</dd>
                   <dd className='mt-1 text-sm text-gray-900'>{userDetails.email}</dd>
                 </div>
                 <div className='sm:col-span-1'>
-                  <dt className='text-sm font-medium text-gray-500'>Salary expectation</dt>
-                  <dd className='mt-1 text-sm text-gray-900'>$120,000</dd>
+                  <dt className='text-md font-medium text-gray-500'>Address</dt>
+                  <dd className='mt-1 text-sm text-gray-900'>{userDetails.address2}</dd>
+                  <dd className='mt-1 text-sm text-gray-900'>{userDetails.city}, {userDetails.state}  {userDetails.zipcode}</dd>
                 </div>
                 <div className='sm:col-span-2'>
-                  <dt className='text-sm font-medium text-gray-500'>About</dt>
-                  <dd className='mt-1 text-sm text-gray-900'>
-                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-                    qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-                    pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                  </dd>
+                  <dt className='text-md font-medium text-gray-500'>Notes</dt>
+                  <dd className='mt-1 text-sm text-gray-900'>Availability: {userDetails.availability}</dd>
+                  <dd className='mt-1 text-sm text-gray-900'>Preferred Event Types: {userDetails.preferred_event}</dd>
                 </div>
                 <div className='sm:col-span-2'>
-                  <dt className='text-sm font-medium text-gray-500'>Attachments</dt>
-                  <dd className='mt-1 text-sm text-gray-900'>
-                    <ul role='list' className='border border-gray-200 rounded-md divide-y divide-gray-200'>
-                      <li className='pl-3 pr-4 py-3 flex items-center justify-between text-sm'>
-                        <div className='w-0 flex-1 flex items-center'>
-                          <PaperClipIcon className='flex-shrink-0 h-5 w-5 text-gray-400' aria-hidden='true' />
-                          <span className='ml-2 flex-1 w-0 truncate'>resume_back_end_developer.pdf</span>
-                        </div>
-                        <div className='ml-4 flex-shrink-0'>
-                          <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                            Download
-                          </a>
-                        </div>
-                      </li>
-                      <li className='pl-3 pr-4 py-3 flex items-center justify-between text-sm'>
-                        <div className='w-0 flex-1 flex items-center'>
-                          <PaperClipIcon className='flex-shrink-0 h-5 w-5 text-gray-400' aria-hidden='true' />
-                          <span className='ml-2 flex-1 w-0 truncate'>coverletter_back_end_developer.pdf</span>
-                        </div>
-                        <div className='ml-4 flex-shrink-0'>
-                          <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                            Download
-                          </a>
-                        </div>
-                      </li>
-                    </ul>
-                  </dd>
+                  <dt className='text-md mb-2 font-medium text-gray-500'>Volunteer Intake Status</dt>
+                  <VolunteerIntakeStatus token={token} userDetails={userDetails} />
+
                 </div>
               </dl>
             </div>
